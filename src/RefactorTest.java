85d9f66fb2ac7eac629baa59a798f30a2c2afae0
Lukas Markoc <lukass.markovic@gmail.com>
2016-05-09 21:29:43 +0200

    RafactorFix

--- src/RefactorTest.java
+++ src/RefactorTest.java
@@ -1,19 +1,23 @@
 public class RefactorTest {
 
-	public static void main(String[] args) {
+	//REFACTOR Long Parameter List
+    public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
 	
-	private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
+	//REFACTOR Long Parameter List
+    private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
 		try {
 			System.out.println("Testing");
-		} catch(Exception e) {
+		} //REFACTOR Catch and rethrow
+    catch(Exception e) {
 			
 		}
 		
 		try {
 			System.out.println("Testing2");
-		} catch(Exception e) {
+		} //REFACTOR Catch and rethrow
+    catch(Exception e) {
 			throw e;
 		}
 	}
