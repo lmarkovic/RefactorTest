c8861ef6a83b5cacab2a26a548e7cd039b9e18e2
Lukas Markoc <lukass.markovic@gmail.com>
2016-05-09 12:57:00 +0200

    Added testing method

--- src/RefactorTest.java
+++ src/RefactorTest.java
@@ -3,4 +3,18 @@ public class RefactorTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
+	
+	private void managePerson(String name, String surrName, int age, int id, int id_card, String city, String address) {
+		try {
+			System.out.println("Testing");
+		} catch(Exception e) {
+			
+		}
+		
+		try {
+			System.out.println("Testing2");
+		} catch(Exception e) {
+			throw e;
+		}
+	}
 }
